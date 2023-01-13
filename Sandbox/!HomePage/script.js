5e411f9f7e076e7078d699418efcc1fc0caef9cd
Albert1Steen <gijsdijk@outlook.com>
2023-01-13 09:43:15 -0800

    Add files via upload

---
+++ Sandbox/!HomePage/script.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll(".menu-button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", event => {
+    switch (event.target.textContent) {
+        case "Home":
+            window.location.href = "file:///C:/VSCode/DysonSphereProgram/Sandbox/!HomePage/HomePage.html";
+            break;
+        case "Low Tier":
+            window.location.href = "file:///C:/VSCode/DysonSphereProgram/Sandbox/1Low-Tier/Low-Tier.html";
+            break;
+        case "Mid Tier":
+            window.location.href = "file:///C:/VSCode/DysonSphereProgram/Sandbox/2Mid-Tier/Mid-Tier.html";
+            break;
+        case "High Tier":
+            window.location.href = "file:///C:/VSCode/DysonSphereProgram/Sandbox/3High-Tier/High-Tier.html";
+            break;
+        case "Science":
+            window.location.href = "file:///C:/VSCode/DysonSphereProgram/Sandbox/4Science/Science.html";
+            break;
+        case "Buildings":
+            window.location.href = "file:///C:/VSCode/DysonSphereProgram/Sandbox/5Buildings/Buildings.html";
+            break;
+      }
+  });
+});
